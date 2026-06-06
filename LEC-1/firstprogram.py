408828c16734b380416b7b7906d2ed95880d878c
rimish1108 <rimishsrivastava3@gmail.com>
2026-06-06 13:31:25 +0530

    Upadate a file

--- LEC-1/firstprogram.py
+++ LEC-1/firstprogram.py
@@ -1,76 +1,76 @@
 # #STARTING A PYTHON JOURNEY
 # # PRINT STATEMENT
-# print("Hello world!")
-# print("My Name is Rimish Chandra Srivastava")
-# print("My age is 21")
+print("Hello world!")
+print("My Name is Rimish Chandra Srivastava")
+print("My age is 21")
 
-# print("My name is Rimish Chandra Srivastava and my age is 21", "My age is 23")
+print("My name is Rimish Chandra Srivastava and my age is 21", "My age is 23")
 
-# print(23)
-# print(13)
-# print(13+23)
-# print(58)
+print(23)
+print(13)
+print(13+23)
+print(58)
 
 # VARIABLES
 
-# name = "Rimish Chandra Srivastava" # String 
-# age = 21 # Integer
-# price = 25.99 # Float
-# age2 = age
-# # print(name)
-# print(age)
-# print(price)
+name = "Rimish Chandra Srivastava" # String 
+age = 21 # Integer
+price = 25.99 # Float
+age2 = age
+# print(name)
+print(age)
+print(price)
 
-# print("My name is :", name)
-# print("My age is :", age)
-# print(age2)
+print("My name is :", name)
+print("My age is :", age)
+print(age2)
 
 # DATA TYPES => Integer
 
-# num = 33
-# num1 = 212
-# num2 = 1000
+num = 33
+num1 = 212
+num2 = 1000
 
-# print(type(num))
-# print(type(num1))
-# print(type(num2))
-# print(num)
-# print(num1)
-# print(num2)
+print(type(num))
+print(type(num1))
+print(type(num2))
+print(num)
+print(num1)
+print(num2)
 
-# print(num + num1 + num2)
+print(num + num1 + num2)
 
 
 # DATA TYPES => String
 
-# print(type(name))
-# print(type(age))
-# print(type(price))
+print(type(name))
+print(type(age))
+print(type(price))
 
-# name1 = "Rimish"
-# name2 = 'Chandra'
-# name3 = """Srivastava"""
+name1 = "Rimish"
+name2 = 'Chandra'
+name3 = """Srivastava"""
 
-# print(name1)
-# print(name2)
-# print(name3)
+print(name1)
+print(name2)
+print(name3)
 
 # DATA TYPES => Float
 
-# salary = 500000.34
+salary = 500000.34
 
-# print(type(salary))
-# print(salary)
+print(type(salary))
+print(salary)
 
 # DATA TYPES => Boolean
 
-# value = True
-# Value1 = False
+value = True
+Value1 = False
 
-# print(type(value))
-# print(type(Value1))
-# print(value)
-# print(Value1)
+print(type(value))
+print(type(Value1))
+print(value)
+print(Value1)
 
 # DATA TYPES => None
  

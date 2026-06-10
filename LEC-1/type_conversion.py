1a8442d8c97ef6a35b31fe804a73bbc3e9ae7a28
rimish1108 <rimishsrivastava3@gmail.com>
2026-06-10 16:22:23 +0530

    Input in Python

--- LEC-1/type_conversion.py
+++ LEC-1/type_conversion.py
@@ -1,16 +1,29 @@
-# Type Conversion
-# a = "2"
-# b = 4.25
-# sum = a + b
-# print(sum)
-# print(type(sum))
+# # # Type Conversion
+# # # a = "2"
+# # # b = 4.25
+# # # sum = a + b
+# # # print(sum)
+# # # print(type(sum))
 
-# Type Casting 
-a,b=1,"2"
+# # # Type Casting 
+# # a,b=1,"2"
 
-b=int(b)
-sum = a+b
-print(sum)
-print(type(sum))
-print(b)
-print(type(b))
+# # b=int(b)
+# # sum = a+b
+# # print(sum)
+# # print(type(sum))
+# # print(b)
+# # print(type(b))
+
+
+# # INPUT IN PYTHON
+
+# Name =input("Enter your name: ")
+# print("Welcome", Name)
+
+name = input("Enter your Name :")
+age = int(input("Enter you current age :"))
+print("Welcome Back !", name)
+print("Your current age is :", age)
+print(type(name), name)
+print(type(age), age)

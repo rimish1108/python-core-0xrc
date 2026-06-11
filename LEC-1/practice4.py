d25f7f67a3d31134176d3fbf7070acf2762b0af2
rimish1108 <rimishsrivastava3@gmail.com>
2026-06-11 10:00:52 +0530

    practice question 4

---
+++ LEC-1/practice4.py
@@ -0,0 +1,5 @@
+# Write a program to input int number a & b, print true if a is greater than or equal to b. if it not false.
+a = int(input("Enter First Number:"))
+b = int(input("Enter Second Number:"))
+
+print("it is", a>=b, "and the value of a is: ", a)

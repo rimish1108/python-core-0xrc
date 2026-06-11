b3cb3bc0f5b1c74ea6a73a231dbd83a03f33163a
rimish1108 <rimishsrivastava3@gmail.com>
2026-06-11 09:38:04 +0530

    updated type_conversion.py

--- LEC-1/type_conversion.py
+++ LEC-1/type_conversion.py
@@ -1,29 +1,36 @@
-# # # Type Conversion
-# # # a = "2"
-# # # b = 4.25
-# # # sum = a + b
+# # # # Type Conversion
+# # # # a = "2"
+# # # # b = 4.25
+# # # # sum = a + b
+# # # # print(sum)
+# # # # print(type(sum))
+
+# # # # Type Casting 
+# # # a,b=1,"2"
+
+# # # b=int(b)
+# # # sum = a+b
 # # # print(sum)
 # # # print(type(sum))
-
-# # # Type Casting 
-# # a,b=1,"2"
-
-# # b=int(b)
-# # sum = a+b
-# # print(sum)
-# # print(type(sum))
-# # print(b)
-# # print(type(b))
+# # # print(b)
+# # # print(type(b))
 
 
-# # INPUT IN PYTHON
+# # # INPUT IN PYTHON
 
-# Name =input("Enter your name: ")
-# print("Welcome", Name)
+# # Name =input("Enter your name: ")
+# # print("Welcome", Name)
 
-name = input("Enter your Name :")
-age = int(input("Enter you current age :"))
-print("Welcome Back !", name)
-print("Your current age is :", age)
-print(type(name), name)
-print(type(age), age)
+# name = input("Enter your Name :")
+# age = int(input("Enter you current age :"))
+# print("Welcome Back !", name)
+# print("Your current age is :", age)
+# print(type(name), name)
+# print(type(age), age)
+
+name = input("Enter your name:")
+age = int(input("Enter your current age: "))
+marks = int(input("Enter your total marks obtained: "))
+print("Welcome to the private section!", name)
+print("Your Current age is :", age)
+print("The marks you obtained in your finals are: ", marks)

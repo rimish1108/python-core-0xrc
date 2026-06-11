6b4a18cf292e6bf3596a8b5564dab5010f64743f
rimish1108 <rimishsrivastava3@gmail.com>
2026-06-11 09:52:55 +0530

    practice question 3

---
+++ LEC-1/practice3.py
@@ -0,0 +1,5 @@
+# Write a program to input 2 floating point numbers and print their Average.
+num1 = float(input("Enter the Decimal number:"))
+num2 = float(input("Enter the Decimal number:"))
+Average = (num1 + num2) / 2
+print(Average)

7fa515568f556b8ce6bac0ccf5bd00cb44db36f4
rimish1108 <rimishsrivastava3@gmail.com>
2026-06-11 16:09:36 +0530

    Lec-2 Practice question 2

---
+++ LEC-2/practice2.py
@@ -0,0 +1,3 @@
+# Write a progra to find the occurrence of $ in a string.
+str2 = "Hello everyone i am $ , My name is $, i am $ currency, i am a currency of america. In america people call me $(dollar). Eveyone loves $ me. most of the women do sex for $. "
+print(str2.count("$"))

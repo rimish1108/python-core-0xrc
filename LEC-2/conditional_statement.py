66774f7c033f02924bad174bc4d6ffd0d86584d3
rimish1108 <rimishsrivastava3@gmail.com>
2026-06-16 00:11:39 +0530

    Updating Conditional statements

---
+++ LEC-2/conditional_statement.py
@@ -0,0 +1,27 @@
+# CONDITIONAL STATEMENT 
+# IF STATEMENT
+
+# age = 24
+
+# if(age >= 18):
+#     print("You are elegible to give vote")
+
+
+# print("****End of code****")
+
+# if(True):
+#     print("You can vote. Vote sincerly and carefully")
+
+
+# ELIF STATEMENT
+# TRAFFIC LIGHT CODE
+
+light = input("Enter the color of light : ")
+if(light == "red"):
+    print("Please Stop")
+elif(light == "green"):
+    print("Now you can go, Drive slowly and safely")
+elif(light == "yellow"):
+    print("Hey! stop and look towards the countdown.")
+
+print("End of the code")

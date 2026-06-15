ec813c21d47634d7f56ccbdb05095e6629f323da
rimish1108 <rimishsrivastava3@gmail.com>
2026-06-16 01:08:35 +0530

    Nested IF

--- LEC-2/conditional_statement.py
+++ LEC-2/conditional_statement.py
@@ -1,29 +1,39 @@
-# CONDITIONAL STATEMENT 
-# IF STATEMENT
+# # CONDITIONAL STATEMENT 
+# # IF STATEMENT
 
-# age = 24
+# # age = 24
 
-# if(age >= 18):
-#     print("You are elegible to give vote")
-
-
-# print("****End of code****")
-
-# if(True):
-#     print("You can vote. Vote sincerly and carefully")
+# # if(age >= 18):
+# #     print("You are elegible to give vote")
 
 
-# ELIF STATEMENT
-# TRAFFIC LIGHT CODE
+# # print("****End of code****")
 
-light = input("Enter the color of light : ")
-if(light == "red"):
-    print("Please Stop")
-elif(light == "green"):
-    print("Now you can go, Drive slowly and safely")
-elif(light == "yellow"):
-    print("Hey! stop and look towards the countdown.")
+# # if(True):
+# #     print("You can vote. Vote sincerly and carefully")
+
+
+# # ELIF STATEMENT
+# # TRAFFIC LIGHT CODE
+
+# light = input("Enter the color of light : ")
+# if(light == "red"):
+#     print("Please Stop")
+# elif(light == "green"):
+#     print("Now you can go, Drive slowly and safely")
+# elif(light == "yellow"):
+#     print("Hey! stop and look towards the countdown.")
+# else:
+#     print("The traffic light is broken, plz contact to the nearest traffice police or a police station for any queries")
+
+# print("End of the code")
+
+# NESTED STATEMENT
+age = int(input("Enter your age: "))
+if(age >= 18):
+    if(age >= 80):
+        print("Cannot drive the car")
+    else:
+        print("Can drive the car")
 else:
-    print("The traffic light is broken, plz contact to the nearest traffice police or a police station for any queries")
-
-print("End of the code")
+    print("can't drive the car")

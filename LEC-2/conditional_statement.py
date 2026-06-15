b3ae9f02605705f84c5ed33f3822f74c59229dec
rimish1108 <rimishsrivastava3@gmail.com>
2026-06-16 01:13:35 +0530

    removing program from comments

--- LEC-2/conditional_statement.py
+++ LEC-2/conditional_statement.py
@@ -1,32 +1,32 @@
 # # CONDITIONAL STATEMENT 
 # # IF STATEMENT
 
-# # age = 24
+age = 24
 
-# # if(age >= 18):
-# #     print("You are elegible to give vote")
+if(age >= 18):
+    print("You are elegible to give vote")
 
 
-# # print("****End of code****")
+print("****End of code****")
 
-# # if(True):
-# #     print("You can vote. Vote sincerly and carefully")
+if(True):
+    print("You can vote. Vote sincerly and carefully")
 
 
 # # ELIF STATEMENT
 # # TRAFFIC LIGHT CODE
 
-# light = input("Enter the color of light : ")
-# if(light == "red"):
-#     print("Please Stop")
-# elif(light == "green"):
-#     print("Now you can go, Drive slowly and safely")
-# elif(light == "yellow"):
-#     print("Hey! stop and look towards the countdown.")
-# else:
-#     print("The traffic light is broken, plz contact to the nearest traffice police or a police station for any queries")
+light = input("Enter the color of light : ")
+if(light == "red"):
+    print("Please Stop")
+elif(light == "green"):
+    print("Now you can go, Drive slowly and safely")
+elif(light == "yellow"):
+    print("Hey! stop and look towards the countdown.")
+else:
+    print("The traffic light is broken, plz contact to the nearest traffice police or a police station for any queries")
 
-# print("End of the code")
+print("End of the code")
 
 # NESTED STATEMENT
 age = int(input("Enter your age: "))

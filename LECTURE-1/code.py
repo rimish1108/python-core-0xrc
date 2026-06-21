0585060354b52683039be16ce989e0e9971b22b0
rimish1108 <rimishsrivastava3@gmail.com>
2026-06-21 07:27:09 +0530

    Updated Studet details

--- LECTURE-1/code.py
+++ LECTURE-1/code.py
@@ -2,16 +2,27 @@ class Student:
     def __init__(self, name, gender, course, year, CGPA):
         self.name = name
         self.gender = gender
-        self.course =course
+        self.course = course
         self.year = year
         self.CGPA = CGPA
-    
+
     def showDetails(self):
-        print("Your name: ", self.name)
-        print("Your gender: ", self.gender)
-        print("Your course ", self.course)
-        print("Your year: ", self.year)
-        print("Your CGPA: ", self.CGPA)
-    
-stu1 = Student("Rimish Chandra Srivastava", "Male", "BTech", 4, 8.8)
+        print("\n--- Student Details ---")
+        print("Your name:", self.name)
+        print("Your gender:", self.gender)
+        print("Your course:", self.course)
+        print("Your year:", self.year)
+        print("Your CGPA:", self.CGPA)
+
+# Taking input from the user
+name = input("Enter your name: ")
+gender = input("Enter your gender: ")
+course = input("Enter your course: ")
+year = int(input("Enter your year: "))
+CGPA = float(input("Enter your marks: "))
+
+# Creating object
+stu1 = Student(name, gender, course, year, CGPA)
+
+# Displaying details
 stu1.showDetails()
